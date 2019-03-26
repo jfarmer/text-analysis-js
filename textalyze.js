debde99dbab24f5680ddf1cc480015ca2b04b123
diegodsgarcia <diego.ds.garcia@gmail.com>
2019-03-26 01:03:53 -0300

    Remove note necessary comments of code

--- textalyze.js
+++ textalyze.js
@@ -1,12 +1,4 @@
 /**
- * This is the base code for v0.1 of our JavaScript text analyzer.
- * Visit https://github.com/jfarmer/text-analysis to see what to do.
- *
- * Send a message in Slack if you're stuck or unsure what to do.  These
- * comments are here to help you, but please delete them as you go along.
- */
-
-/**
  * Given an input Array, returns a Map containing the count of each item in the input.
  * @param {Array} array - The array of items to count
  * @param {Map} counts - A Map containing the counts of the items in the input array
@@ -34,5 +26,5 @@ console.log(`The counts for [${items}] are...`);
 
 itemCounts(items).forEach((value, key) => {
   console.log(`${value}  ${key}`);
-})
+});
 
