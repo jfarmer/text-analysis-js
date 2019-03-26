f90c4d3d4eda607038449d53013fdb893dddfe4d
diegodsgarcia <diego.ds.garcia@gmail.com>
2019-03-26 00:58:40 -0300

    Implementation of itemCounts and test hard code result

--- textalyze.js
+++ textalyze.js
@@ -11,13 +11,28 @@
  * @param {Array} array - The array of items to count
  * @param {Map} counts - A Map containing the counts of the items in the input array
  */
-function itemCounts(array) {
-  let counts = new Map();
 
-  // Your code here.
-  // Run 'npm test' to see what tests need to pass.
+function itemCounts(array) {
+  const counts = new Map();
+
+  array.forEach((item) => {
+    if (counts.has(item)) {
+      counts.set(item, counts.get(item) + 1);
+    } else {
+      counts.set(item, 1);
+    }
+  });
 
   return counts;
 }
 
 module.exports = { itemCounts };
+
+// Result
+const items = ['a', 'a', 'a', 'b', 'b', 'c'];
+console.log(`The counts for [${items}] are...`); 
+
+itemCounts(items).forEach((value, key) => {
+  console.log(`${value}  ${key}`);
+})
+
