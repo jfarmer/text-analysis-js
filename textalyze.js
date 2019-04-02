7052bf401d9b40766d7005df607b036eeddaaff5
Robson da Rocha <robsonndarocha@gmail.com>
2019-04-02 20:17:04 +0000

    [v0.2] String to Characters

--- textalyze.js
+++ textalyze.js
@@ -13,11 +13,37 @@
  */
 function itemCounts(array) {
   const counts = new Map();
+  var element = "";
+  var count = 0;
 
   // Your code here.
   // Run 'npm test' to see what tests need to pass.
+  for(var i = 0; i < array.length; i++) {
+  	element = array[i];
+  	for(var j = 0; j < array.length; j++) {
+  		if(array[j] === element) {
+  			count++;
+  		}
+  	}
+  	counts.set(element, count);
+  	count = 0;
+  }
 
   return counts;
 }
+
+function toChar(string) {
+	return string.slice();
+}
+
+var charArray = toChar("Olá! Meu nome é Robson.");
+
+var charCounts = itemCounts(charArray);
+
+function logMapElements(value, key, map) {
+  console.log(`char[${key}] = ${value} ocurrence(s)`);
+}
+
+charMap.forEach(logMapElements);
 
 module.exports = { itemCounts };
