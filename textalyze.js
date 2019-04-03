4f6c795dd71a3c1c5eae9e89e7657e0b35b1e162
Robson da Rocha <robsonndarocha@gmail.com>
2019-04-03 17:50:10 +0000

    [v1.0] Reading From a User-Supplied File

--- textalyze.js
+++ textalyze.js
@@ -1,10 +1,22 @@
-/**
- * This is the base code for v0.1 of our JavaScript text analyzer.
- * Visit https://github.com/jfarmer/text-analysis to see what to do.
- *
- * Send a message in Slack if you're stuck or unsure what to do.  These
- * comments are here to help you, but please delete them as you go along.
- */
+function userInput() {
+  return process.argv[2];
+}
+
+function readFromFile(string) {
+  const fs = require('fs');
+  const path = require('path');
+  var filePath = path.join(__dirname, 'sample_data', string);
+  var content = fs.readFileSync(filePath).toString();
+  return content;
+}
+
+function sanitize(string) {
+  return string.toLowerCase();
+}
+
+function toChar(string) {
+  return string.slice();
+}
 
 /**
  * Given an input Array, returns a Map containing the count of each item in the input.
@@ -16,33 +28,31 @@ function itemCounts(array) {
   var element = "";
   var count = 0;
 
-  // Your code here.
-  // Run 'npm test' to see what tests need to pass.
   for(var i = 0; i < array.length; i++) {
-  	element = array[i];
-  	for(var j = 0; j < array.length; j++) {
-  		if(array[j] === element) {
-  			count++;
-  		}
-  	}
-  	counts.set(element, count);
-  	count = 0;
+    element = array[i];
+    for(var j = 0; j < array.length; j++) {
+      if(array[j] === element) {
+        count++;
+      }
+    }
+    counts.set(element, count);
+    count = 0;
   }
 
   return counts;
 }
 
-function toChar(string) {
-	return string.slice();
-}
-
-var charArray = toChar("Olá! Meu nome é Robson.");
-
-var charCounts = itemCounts(charArray);
-
 function logMapElements(value, key, map) {
   console.log(`char[${key}] = ${value} ocurrence(s)`);
 }
+
+var fileToString = readFromFile(userInput());
+
+var sanitizedString = sanitize(fileToString);
+
+var charArray = toChar(sanitizedString);
+
+var charCounts = itemCounts(charArray);
 
 charCounts.forEach(logMapElements);
 
